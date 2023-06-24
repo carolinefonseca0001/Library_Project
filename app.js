8d47fdfd169b2c337cc4e2111cf43ba5a6521156
Caroline <caroline.fonseca0001@gmail.com>
2023-06-23 22:03:09 -0400

    First crack

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
-console.log("Hello World!\n==========\n");
-
-// PROJECT Section
 console.log("PROJECT:\n==========\n");
+
+class Book {
+  constructor(title, author, read) {
+    this.title = title;
+    this.author = author;
+    this.read = read;
+  }
+}
+
+let book = new Book("The Wizard of Oz", "L Frank Baum", true);
+
+class Library {
+  constructor() {
+    const books = [
+      "The Wizard of Oz",
+      "The Great Gatsby",
+      "Night Film",
+      "Little Women",
+    ];
+    const bookCount = 4;
+
+    function markRead(checkbox, id) {
+      if (checkbox == true && id == true) {
+      } else {
+      }
+    }
+  }
+  // const x = new Boolean(false);
+  // if (x) {
+  //   // this code is executed
+}
+const books = [];
+books[0] = "Oz";
+books[1] = "Gatsby";
+books[2] = "Little";
