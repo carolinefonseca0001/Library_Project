4e2e4ce1060317ea6aa761fd98c7ba4f213bb83e
Caroline <caroline.fonseca0001@gmail.com>
2023-06-25 18:29:44 -0400

    README.md lines 36 - on, I have no clue what I'm doing.

--- app.js
+++ app.js
@@ -4,33 +4,36 @@ class Book {
   constructor(title, author, read) {
     this.title = title;
     this.author = author;
-    this.read = read;
+    this.read = Boolean(read);
   }
 }
 
-let book = new Book("The Wizard of Oz", "L Frank Baum", true);
+let Library = [];
+let counter = 0;
 
-class Library {
-  constructor() {
-    const books = [
-      "The Wizard of Oz",
-      "The Great Gatsby",
-      "Night Film",
-      "Little Women",
-    ];
-    const bookCount = 4;
-
-    function markRead(checkbox, id) {
-      if (checkbox == true && id == true) {
-      } else {
-      }
-    }
+function markRead(checkbox, value) {
+  let bookTitle = title.value;
+  let bookAuthor = author.value;
+  let bookPages = pages.value;
+  let bookRead;
+  if (title.value == value && checkbox.checked) {
+    bookRead = Boolean(true);
+  } else {
+    bookRead = Boolean(false);
   }
-  // const x = new Boolean(false);
-  // if (x) {
-  //   // this code is executed
 }
-const books = [];
-books[0] = "Oz";
-books[1] = "Gatsby";
-books[2] = "Little";
+
+function addBook(book) {
+  Library.push(book);
+}
+
+//README.md lines 36 - on, I have no clue what I'm doing.
+
+var x = document.createElement("TABLE");
+let TABLE = [{ name: "Title" }, { name: "Author" }, { name: "Read" }];
+
+document.getElementById("submit").addEventListener("click", displayDate);
+
+function displayDate() {
+  document.getElementById("demo").innerHTML = Date();
+}
