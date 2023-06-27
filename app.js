af93d88b97ff3a990b738395bd52185684bd790a
Caroline <caroline.fonseca0001@gmail.com>
2023-06-26 20:54:17 -0400

    Worked on exercises

--- app.js
+++ app.js
@@ -7,33 +7,51 @@ class Book {
     this.read = Boolean(read);
   }
 }
-
-let Library = [];
-let counter = 0;
-
-function markRead(checkbox, value) {
-  let bookTitle = title.value;
-  let bookAuthor = author.value;
-  let bookPages = pages.value;
-  let bookRead;
-  if (title.value == value && checkbox.checked) {
-    bookRead = Boolean(true);
-  } else {
-    bookRead = Boolean(false);
+class Library {
+  constructor() {
+    this.count = 0;
+    this.bookList = [];
+  }
+  markRead(checkbox, id) {
+    let bookTitle = title.id;
+    let bookAuthor = author.id;
+    let bookPages = pages.id;
+    let bookRead;
+    if (title.value == value && checkbox.checked) {
+      bookRead = Boolean(true);
+    } else {
+      bookRead = Boolean(false);
+    }
+  }
+  addBook(book) {
+    this.bookList.push(book);
+    this.count++;
   }
 }
 
-function addBook(book) {
-  Library.push(book);
-}
-
+let lib = new Library();
 //README.md lines 36 - on, I have no clue what I'm doing.
 
 var x = document.createElement("TABLE");
 let TABLE = [{ name: "Title" }, { name: "Author" }, { name: "Read" }];
 
 document.getElementById("submit").addEventListener("click", displayDate);
+document.getElementById("myform").addEventListener("submit", addbookhelper);
 
 function displayDate() {
-  document.getElementById("demo").innerHTML = Date();
+  // document.getElementById("demo").innerHTML = Date();
+  console.log("displaydate");
+}
+
+function addbookhelper(event) {
+  event.preventDefault();
+  let userTitle = document.getElementById("title").value;
+  let userAuthor = document.getElementById("author").value;
+  let userRead = document.getElementById("read").checked;
+  console.log(userTitle);
+  console.log(userAuthor);
+  console.log(userRead);
+  let mynewbook = new Book(userTitle, userAuthor, userRead);
+  lib.addBook(mynewbook);
+  console.log("HIYA!!!!");
 }
